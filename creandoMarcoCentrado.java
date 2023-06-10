89e7ea1c0d99b6bb753ef4dc9a587b48660cd4cb
Seth <98616042+Seth0094@users.noreply.github.com>
2023-06-10 13:42:58 -0400

    Update creandoMarcoCentrado.java

--- creandoMarcoCentrado.java
+++ creandoMarcoCentrado.java
@@ -1,28 +1,37 @@
 package graficos;
 import java.awt.Toolkit;
 import java.awt.Dimension;
+import java.awt.Image;
 import javax.swing.JFrame;
 
-//giorgio baglieri 29.599.848
+
+// Giorgio Baglieri 29.599.848
+
 
 public class creandoMarcoCentrado {
+	
 
 	public static void main(String[] args) {
 		marcoCentrado miMarco = new marcoCentrado();
 		miMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		miMarco.setVisible(true);
+		miMarco.setTitle("computacion Grafica");
 	}
 
 }
 
 class marcoCentrado extends JFrame {
 	
+	private static final long serialVersionUID = 1L;
+
 	public marcoCentrado() {
 		Toolkit miPantalla = Toolkit.getDefaultToolkit();
-		Dimension tamañoPantalla = miPantalla.getScreenSize();
-		int alturaPantalla = tamañoPantalla.height;
-		int anchoPantalla = tamañoPantalla.width;
+		Dimension tamaÃ±oPantalla = miPantalla.getScreenSize();
+		int alturaPantalla = tamaÃ±oPantalla.height;
+		int anchoPantalla = tamaÃ±oPantalla.width;
 		setSize(anchoPantalla/2, alturaPantalla/2);
 		setLocation(anchoPantalla/4, alturaPantalla/4);
+		Image miIcono = miPantalla.getImage("Images.png");
+		setIconImage(miIcono);
 	}
 }
